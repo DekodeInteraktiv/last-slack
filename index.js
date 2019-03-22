37d98bbdf52d502be488d847a50c197be261ba3a
Jerome Lachaud <jerome.lachaud@gmail.com>
2019-03-22 23:00:29 +0100

    Code styling

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const commander = require('commander');
+const commander = require('commander')
 
-const checkIfTrackIsPlayingAndNew = require('./lib/checkIfTrackIsPlayingAndNew');
-const fetchLastPlayedTrack = require('./lib/fetchLastPlayedTrack');
+const checkIfTrackIsPlayingAndNew = require('./lib/checkIfTrackIsPlayingAndNew')
+const fetchLastPlayedTrack = require('./lib/fetchLastPlayedTrack')
 
 // Parse args
 commander
   .version('1.0.0')
   .usage('[options] <username>')
-  .option('-q, --quiet', 'Don\'t send message to Slack')
-  .parse(process.argv);
+  .option('-q, --quiet', "Don't send message to Slack")
+  .parse(process.argv)
 
 if (!commander.args.length) {
-  commander.help();
+  commander.help()
 }
 
-fetchLastPlayedTrack(commander.args[0], checkIfTrackIsPlayingAndNew);
+fetchLastPlayedTrack(commander.args[0], checkIfTrackIsPlayingAndNew)
